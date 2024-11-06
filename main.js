4294210886eca7c7975f9bd257e804006dc37b60
GuillermoAAD <guillermo.alcaraz@perspective.com.mx>
2024-11-06 17:49:57 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+
+console.log('Hello from Electron 👋')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+  // win.loadURL('https://web.whatsapp.com/')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
