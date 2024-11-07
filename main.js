8f108ac03fb3a5d9973503de0092cd8653b36af6
GuillermoAAD <guillermo.alcaraz@perspective.com.mx>
2024-11-07 16:38:47 -0600

    [+] Comunicación entre procesos

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const { app, BrowserWindow } = require('electron/main')
+// main.js
+
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('node:path')
 
 
-console.log('Hello from Electron 👋')
+// console.log('Hello from Electron 👋')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true, // Habilita esto para mayor seguridad
+      nodeIntegration: false, // Deshabilita nodeIntegration para seguir buenas prácticas
     }
   })
 
@@ -18,13 +22,14 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+  // app.on('activate', () => {
+  //   if (BrowserWindow.getAllWindows().length === 0) {
+  //     createWindow()
+  //   }
+  // })
 })
 
 app.on('window-all-closed', () => {
